63a19fb9a0e7411988eb18b399e7dc6aa5cc6b15
Konrad Koryński <kondzio6@gmail.com>
2019-10-15 23:51:20 +0200

    Add project files.

---
+++ MultiLocalDeploy/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace MultiLocalDeploy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fbdTargetFolders = new System.Windows.Forms.FolderBrowserDialog();
+            this.fbdSourceFolder = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnTarget = new System.Windows.Forms.Button();
+            this.btnSource = new System.Windows.Forms.Button();
+            this.lblSource = new System.Windows.Forms.Label();
+            this.btnDeploy = new System.Windows.Forms.Button();
+            this.tbLog = new System.Windows.Forms.TextBox();
+            this.pbDeploy = new System.Windows.Forms.ProgressBar();
+            this.lbFolders = new System.Windows.Forms.ListBox();
+            this.cbSkipConfigFiles = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // fbdSourceFolder
+            // 
+            this.fbdSourceFolder.ShowNewFolderButton = false;
+            // 
+            // btnTarget
+            // 
+            this.btnTarget.Location = new System.Drawing.Point(225, 14);
+            this.btnTarget.Name = "btnTarget";
+            this.btnTarget.Size = new System.Drawing.Size(75, 23);
+            this.btnTarget.TabIndex = 1;
+            this.btnTarget.Text = "Target...";
+            this.btnTarget.UseVisualStyleBackColor = true;
+            this.btnTarget.Click += new System.EventHandler(this.BtnTarget_Click);
+            // 
+            // btnSource
+            // 
+            this.btnSource.Location = new System.Drawing.Point(13, 13);
+            this.btnSource.Name = "btnSource";
+            this.btnSource.Size = new System.Drawing.Size(75, 23);
+            this.btnSource.TabIndex = 2;
+            this.btnSource.Text = "Źródło...";
+            this.btnSource.UseVisualStyleBackColor = true;
+            this.btnSource.Click += new System.EventHandler(this.BtnSource_Click);
+            // 
+            // lblSource
+            // 
+            this.lblSource.AutoSize = true;
+            this.lblSource.Location = new System.Drawing.Point(13, 44);
+            this.lblSource.MaximumSize = new System.Drawing.Size(200, 0);
+            this.lblSource.Name = "lblSource";
+            this.lblSource.Size = new System.Drawing.Size(36, 13);
+            this.lblSource.TabIndex = 3;
+            this.lblSource.Text = "Empty";
+            // 
+            // btnDeploy
+            // 
+            this.btnDeploy.BackColor = System.Drawing.Color.Red;
+            this.btnDeploy.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnDeploy.Location = new System.Drawing.Point(454, 359);
+            this.btnDeploy.Name = "btnDeploy";
+            this.btnDeploy.Size = new System.Drawing.Size(334, 79);
+            this.btnDeploy.TabIndex = 4;
+            this.btnDeploy.Text = "DEPLOY";
+            this.btnDeploy.UseVisualStyleBackColor = false;
+            this.btnDeploy.Click += new System.EventHandler(this.BtnDeploy_Click);
+            // 
+            // tbLog
+            // 
+            this.tbLog.Cursor = System.Windows.Forms.Cursors.Default;
+            this.tbLog.Location = new System.Drawing.Point(455, 44);
+            this.tbLog.Multiline = true;
+            this.tbLog.Name = "tbLog";
+            this.tbLog.ReadOnly = true;
+            this.tbLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbLog.Size = new System.Drawing.Size(333, 234);
+            this.tbLog.TabIndex = 6;
+            // 
+            // pbDeploy
+            // 
+            this.pbDeploy.Location = new System.Drawing.Point(455, 284);
+            this.pbDeploy.Name = "pbDeploy";
+            this.pbDeploy.Size = new System.Drawing.Size(333, 23);
+            this.pbDeploy.TabIndex = 7;
+            // 
+            // lbFolders
+            // 
+            this.lbFolders.FormattingEnabled = true;
+            this.lbFolders.Location = new System.Drawing.Point(225, 44);
+            this.lbFolders.Name = "lbFolders";
+            this.lbFolders.Size = new System.Drawing.Size(223, 394);
+            this.lbFolders.TabIndex = 8;
+            this.lbFolders.KeyUp += new System.Windows.Forms.KeyEventHandler(this.lbFolders_KeyUp);
+            // 
+            // cbSkipConfigFiles
+            // 
+            this.cbSkipConfigFiles.AutoSize = true;
+            this.cbSkipConfigFiles.Checked = true;
+            this.cbSkipConfigFiles.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cbSkipConfigFiles.Location = new System.Drawing.Point(16, 61);
+            this.cbSkipConfigFiles.Name = "cbSkipConfigFiles";
+            this.cbSkipConfigFiles.Size = new System.Drawing.Size(107, 17);
+            this.cbSkipConfigFiles.TabIndex = 9;
+            this.cbSkipConfigFiles.Text = "Skip *.config files";
+            this.cbSkipConfigFiles.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cbSkipConfigFiles);
+            this.Controls.Add(this.lbFolders);
+            this.Controls.Add(this.pbDeploy);
+            this.Controls.Add(this.tbLog);
+            this.Controls.Add(this.btnDeploy);
+            this.Controls.Add(this.lblSource);
+            this.Controls.Add(this.btnSource);
+            this.Controls.Add(this.btnTarget);
+            this.Name = "Form1";
+            this.Text = "Multi Local Deploy";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.FolderBrowserDialog fbdTargetFolders;
+        private System.Windows.Forms.FolderBrowserDialog fbdSourceFolder;
+        private System.Windows.Forms.Button btnTarget;
+        private System.Windows.Forms.Button btnSource;
+        private System.Windows.Forms.Label lblSource;
+        private System.Windows.Forms.Button btnDeploy;
+        private System.Windows.Forms.TextBox tbLog;
+        private System.Windows.Forms.ProgressBar pbDeploy;
+        private System.Windows.Forms.ListBox lbFolders;
+        private System.Windows.Forms.CheckBox cbSkipConfigFiles;
+    }
+}
+
---
+++ MultiLocalDeploy/Form1.cs
@@ -0,0 +1,146 @@
+﻿using MultiLocalDeploy.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultiLocalDeploy
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string SourceFolderPath { get; private set; }
+
+        private void BtnSource_Click(object sender, EventArgs e)
+        {
+            if (fbdSourceFolder.ShowDialog() == DialogResult.OK)
+            {
+                SourceFolderPath = fbdSourceFolder.SelectedPath;
+                lblSource.Text = SourceFolderPath;
+            }
+        }
+
+        private void BtnTarget_Click(object sender, EventArgs e)
+        {
+            fbdTargetFolders.RootFolder = Environment.SpecialFolder.MyComputer;
+            if (fbdTargetFolders.ShowDialog() == DialogResult.OK)
+            {
+                lbFolders.Items.AddRange(Directory.GetDirectories(fbdTargetFolders.SelectedPath));
+            }
+        }
+
+        private void BtnDeploy_Click(object sender, EventArgs e)
+        {
+            if (!ValidateDeploy())
+                return;
+
+            var result = MessageBox.Show($"Do you want to deploy {lbFolders.SelectedItem}",
+                                         "Warning!",
+                                         MessageBoxButtons.YesNo,
+                                         MessageBoxIcon.Question);
+
+            if (result != DialogResult.Yes)
+                return;
+
+            var pathToDeploy = lbFolders.SelectedItem.ToString();
+            DeployFiles(SourceFolderPath, pathToDeploy);
+            Log("******************");
+            Log("DONE!");
+            Log("******************");
+        }
+
+        private void DeployFiles(string sourceFolder, string pathToDeploy)
+        {
+            var directories = Directory.GetDirectories(sourceFolder, "*",
+                SearchOption.AllDirectories);
+            var files = Directory.GetFiles(sourceFolder, "*.*",
+                SearchOption.AllDirectories);
+            SetProgresBar(directories.Length + files.Length);
+
+            LogClear();
+
+            //Now Create all of the directories
+            //foreach (string dirPath in directories)
+            //{
+            //    Directory.CreateDirectory(dirPath.Replace(sourceFolder, pathToDeploy));
+            //    pbDeploy.Value++;
+            //    Log($"Copy folder {dirPath}");
+            //}
+
+            Parallel.ForEach(directories, (dirPath) =>
+            {
+                Directory.CreateDirectory(dirPath.Replace(sourceFolder, pathToDeploy));
+                //pbDeploy.Value++;
+                //Log($"Copy folder {dirPath}");
+            });
+
+            //Copy all the files & Replaces any files with the same name
+            //foreach (string newPath in files)
+            //{
+            //    if (cbSkipConfigFiles.Checked && newPath.EndsWith("web.config"))
+            //    {
+            //        pbDeploy.Value++;
+            //        continue;
+            //    }
+
+            //    File.Copy(newPath, newPath.Replace(sourceFolder, pathToDeploy), true);
+            //    pbDeploy.Value++;
+            //    Log($"Copy file {newPath}");
+            //}
+
+            Parallel.ForEach(files, (newPath) =>
+            {
+                if (cbSkipConfigFiles.Checked && newPath.EndsWith("web.config"))
+                {
+                    pbDeploy.Value++;
+                    return;
+                }
+
+                File.Copy(newPath, newPath.Replace(sourceFolder, pathToDeploy), true);
+                //pbDeploy.Value++;
+                //Log($"Copy file {newPath}");
+            });
+        }
+
+        private void SetProgresBar(int steps)
+        {
+            pbDeploy.Maximum = steps;
+            pbDeploy.Value = 0;
+        }
+
+        private bool ValidateDeploy()
+        {
+            return !string.IsNullOrEmpty(SourceFolderPath) && lbFolders.SelectedItem != null;
+        }
+
+        private void Log(string message)
+        {
+            tbLog.Text += message + Environment.NewLine;
+            tbLog.Refresh();
+        }
+
+        private void LogClear()
+        {
+            tbLog.Clear();
+        }
+
+        private void lbFolders_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Delete)
+            {
+                lbFolders.Items.RemoveAt(lbFolders.SelectedIndex);
+            }
+        }
+    }
+}
